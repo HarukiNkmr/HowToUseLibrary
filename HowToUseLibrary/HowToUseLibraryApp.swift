fb78a0eae49035f7bb4a40af73f40a0e4c74e3cd
HarukiNakamura <haruku_msttb@icloud.com>
2024-08-19 11:30:02 +0900

    Initial Commit

---
+++ HowToUseLibrary/HowToUseLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseLibraryApp.swift
+//  HowToUseLibrary
+//
+//  Created by Haruki Nakamura on 2024/08/19.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
